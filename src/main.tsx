23b7785e6187b0de14f32160e027ca5bf8b9386e
bpa03 <baldassarepugliese@gmail.com>
2022-07-04 02:18:34 -0400

    WeatherProvider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 
+// Store
+import WeatherProvider from '@/context';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WeatherProvider>
+      <App />
+    </WeatherProvider>
   </React.StrictMode>
 );
