67c7ae13a8af883dd7c7fbae94fc4eb7cc43db19
bpa03 <baldassarepugliese@gmail.com>
2022-07-04 15:03:51 -0400

    Base sidebar

--- src/layout/Sidebar/index.tsx
+++ src/layout/Sidebar/index.tsx
@@ -1,5 +1,43 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import { FC } from 'react';
+import { BiTargetLock } from 'react-icons/all';
+// Services
+import { Current } from '@/services/Weather/interfaces';
+// Hooks
+import { useStore } from '@/context';
+// Generic components
+import Button from '@/components/Button';
+// Assets
+import bgImage from 'assets/Cloud-background.png';
+
+interface SidebarProps {
+  current: Current;
+}
+
+// Styles
+import { Aside, Container, Image } from './styles';
+
+const Sidebar: FC<SidebarProps> = () => {
+  const store = useStore();
+
+  const { weather } = store;
+  const { location, current } = weather;
+
+  return (
+    <Aside>
+      <Image src={bgImage} />
+      <Container>
+        <div>
+          <Button>Search for places</Button>
+          <Button type="icon">
+            <BiTargetLock
+              title="Target"
+              size={22}
+            />
+          </Button>
+        </div>
+      </Container>
+    </Aside>
+  );
 };
 
 export default Sidebar;
---
+++ src/layout/Sidebar/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Aside = styled.aside`
+  background-color: var(--sidebar-bg-color);
+  padding: 2.556rem 2.333rem;
+  position: relative;
+`;
+
+export const Image = styled.img`
+  position: absolute;
+  top: 39%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  opacity: .05;
+  width: auto;
+`;
+
+export const Container = styled.div`
+  
+`;
