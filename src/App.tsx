911fbc8d6a9add6a5516d169d963a6184f2b47b6
bpa03 <baldassarepugliese@gmail.com>
2022-07-04 22:38:44 -0400

    Basic data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
+import { FC, useEffect } from 'react';
 // Layout Components
 import Sidebar from '@/layout/Sidebar';
 import Dashboard from '@/layout/Dashboard';
-
+// Hooks
+import { useDispatch } from '@/context';
+// Services
+import WeatherAPI from '@/services/Weather';
+import GeoAPI from '@/services/Geolocation';
+// Action creators
+import { getWeather, setWeather } from '@/context/weather/weather.actions';
 // App Styles
 import { Wrapper } from '@/AppStyles';
 import GlobalStyles from '@/GlobalStyles';
 
-const App = () => {
+const App: FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const request = async () => {
+      dispatch(getWeather());
+      try {
+        const coords = await GeoAPI.getGeolocation();
+        if (coords) {
+          const { latitude, longitude } = coords;
+          const weather = await WeatherAPI.getForecastFromCoords(
+            latitude,
+            longitude
+          );
+          return dispatch(setWeather(weather));
+        }
+        const weather = await WeatherAPI.getForecastFromCity('London');
+        return dispatch(setWeather(weather));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    request();
+  }, []);
+
   return (
     <Wrapper>
       <Sidebar />
