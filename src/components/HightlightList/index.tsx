bb8ac7ac1b08efaf434ec74d78b25d932c6b5581
bpa03 <baldassarepugliese@gmail.com>
2022-07-06 15:41:49 -0400

    Added props to show api data in hightlight cards

--- src/components/HightlightList/index.tsx
+++ src/components/HightlightList/index.tsx
@@ -1,33 +1,49 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
+// Interfaces
+import { Current } from '@/services/Weather/interfaces';
 // components
 import HightlightCard from '../HightlightCard';
 
 // Styles
 import { List, ListTitle } from './styles';
 
-const HightlightList: FC = () => {
+interface HightlightListProps {
+  current: Current;
+}
+
+const HightlightList: FC<HightlightListProps> = ({ current }) => {
+  const data = useMemo(
+    () => ({
+      humidity: current.humidity,
+      wind: current.wind_mph,
+      visibility: current.vis_miles,
+      airPressure: current.pressure_mb,
+    }),
+    [current]
+  );
+  
   return (
     <div>
       <ListTitle>Today’s Hightlights </ListTitle>
       <List>
         <HightlightCard
           title="Wind status"
-          value={7}
+          value={data.wind}
           unit="mph"
         />
         <HightlightCard
           title="Humidity"
-          value={84}
+          value={data.humidity}
           unit="%"
         />
         <HightlightCard
           title="Visibility"
-          value={6.4}
+          value={data.visibility}
           unit="miles"
         />
         <HightlightCard
           title="Air Pressure"
-          value={998}
+          value={data.airPressure}
           unit="mb"
         />
       </List>
