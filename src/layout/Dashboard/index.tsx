2ea467d8f0f589e969e488ba553c33d6aff7fa7e
bpa03 <baldassarepugliese@gmail.com>
2022-07-05 21:14:46 -0400

    Basic dashboard layout component

--- src/layout/Dashboard/index.tsx
+++ src/layout/Dashboard/index.tsx
@@ -1,7 +1,27 @@
-const Dashboard = () => {
+import { FC } from 'react';
+// Components
+import WeatherList from '@/components/WeatherList';
+import HightlightList from '@/components/HightlightList';
+// Hooks
+import { useStore } from '@/context';
+// Styles
+import { Container } from './styles';
+
+const Dashboard: FC = () => {
+  const store = useStore();
+  const { loading, weather } = store;
+  const { forecast } = weather;
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Container>
+      {!loading && Object.keys(weather).length && (
+        <>
+          <WeatherList forecast={forecast.forecastday} />
+          <HightlightList />
+        </>
+      )}
+    </Container>
+  );
+};
 
 export default Dashboard;
---
+++ src/layout/Dashboard/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: var(--dashboard-bg-color);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2.556rem 6rem;
+`;
