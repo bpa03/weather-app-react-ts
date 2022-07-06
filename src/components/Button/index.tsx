7b4803da8242f04959af48e56cd6963c38cdd9f7
bpa03 <baldassarepugliese@gmail.com>
2022-07-05 21:08:31 -0400

    Added props to Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,15 +3,23 @@ import { FC, ReactNode } from 'react';
 interface ButtonProps {
   type?: 'normal' | 'icon' | null;
   children: ReactNode | string;
+  className?: string;
 }
 
 // Styles
 import { Base } from './styles';
 
-const Button: FC<ButtonProps> = ({ type, children }) => {
+const Button: FC<ButtonProps> = ({ type, children, className }) => {
   const buttonType = type ?? 'normal';
 
-  return <Base buttonType={buttonType}>{children}</Base>;
+  return (
+    <Base
+      buttonType={buttonType}
+      className={className ? className : ''}
+    >
+      {children}
+    </Base>
+  );
 };
 
 Button.defaultProps = {
