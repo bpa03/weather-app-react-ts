0d476d91f82a1845cad2ceadc4ed654429b4d7ae
bpa03 <baldassarepugliese@gmail.com>
2022-07-07 02:36:16 -0400

    Thunk to get initial weather in current position

--- src/context/weather/weather.thunk.ts
+++ src/context/weather/weather.thunk.ts
@@ -1,6 +1,9 @@
 import { Dispatch } from '../index';
+// Interfaces
+import { Wheather } from '@/services/Weather/interfaces';
 // Service
 import WeatherAPI from '@/services/Weather';
+import GeoAPI from '@/services/Geolocation';
 // Action creators
 import { getWeather, setWeather } from './weather.actions';
 
@@ -9,6 +12,25 @@ export const thunkGetWeather = async (dispatch: Dispatch, city: string) => {
   try {
     const response = await WeatherAPI.getForecastFromCity(city);
     dispatch(setWeather(response));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const thunkGetInitialWeather = async (dispatch: Dispatch) => {
+  dispatch(getWeather());
+  try {
+    let weather: Wheather;
+    const coords = await GeoAPI.getGeolocation();
+
+    if (coords) {
+      const { latitude, longitude } = coords;
+      weather = await WeatherAPI.getForecastFromCoords(latitude, longitude);
+      return dispatch(setWeather(weather));
+    }
+
+    weather = await WeatherAPI.getForecastFromCity('London');
+    return dispatch(setWeather(weather));
   } catch (error) {
     console.log(error);
   }
